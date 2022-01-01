7a473368914a994f02ef7af434a765e7f80b5e1a
Bizet <liubicai@outlook.com>
2022-01-01 11:35:27 +0800

    添加项目文件。

---
+++ AutoUpdateTest/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AutoUpdaterDotNET;
+
+namespace AutoUpdateTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            AutoUpdater.Start("D:\\TEST\\AutoUpdaterTest.xml");
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
